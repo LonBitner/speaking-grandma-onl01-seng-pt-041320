0fdae22584b047c1fb0bc0594c26de0b6ddad72f
Lon Bitner <lonbitner@gmail.com>
2020-04-16 09:22:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT?"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
-  else phrase == "WHAT DID YOU EAT TODAY?"
+  else phrase == "WHAT?"
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
